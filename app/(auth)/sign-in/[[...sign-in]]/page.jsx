279934cd981534a3a1690b33d58c545538cb1627
Gaurav Davesar <gauravdavesar555@gmail.com>
2025-03-22 13:49:48 +0530

    minor changes on auth page

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -60,12 +60,11 @@ export default function Page() {
                         </a>
 
                         <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                            Welcome to Squid 🦑
+                            Welcome to MockNest
                         </h1>
 
                         <p className="mt-4 leading-relaxed text-gray-500">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-                            quibusdam aperiam voluptatum.
+                            Prepare for your next big opportunity with realistic, AI-driven and expert-led mock interviews. Gain confidence, receive personalized feedback, and sharpen your skills with industry-specific interview simulations.
                         </p>
                     </div>
 
