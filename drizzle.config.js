22e3e960a9a59a5371708cf03f922e1fed7fa179
Gaurav Davesar <gauravdavesar555@gmail.com>
2025-04-07 08:58:13 +0530

    changed_code

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
     dialect: "postgresql",
     schema: "./utils/schema.js",
     dbCredentials: {
-        url: 'postgresql://neondb_owner:npg_HDW8qiVObQv7@ep-winter-wave-a5cjr54m-pooler.us-east-2.aws.neon.tech/mocknest_data?sslmode=require'
+        url: 'postgresql://accounts:npg_LNv4FRsSZP5l@ep-still-union-a55udnpx-pooler.us-east-2.aws.neon.tech/mocknest_data?sslmode=require'
     }
 });
