7e349ef1168bd91e785f0fef23275cfbf048c28b
Gaurav Davesar <gauravdavesar555@gmail.com>
2025-04-27 20:07:13 +0530

    navbar remove

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -12,10 +12,7 @@ function Header() {
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
       <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
       <ul className='hidden md:flex gap-6'>
-        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path == '/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
-        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path == '/dashboard/questions' && 'text-primary font-bold'}`}>Questions</li>
-        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path == '/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li>
-        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path == '/dashboard/how' && 'text-primary font-bold'}`}>How it works</li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path == '/dashboard' && 'text-primary font-bold'}`}></li>
       </ul>
       <UserButton />
     </div>
